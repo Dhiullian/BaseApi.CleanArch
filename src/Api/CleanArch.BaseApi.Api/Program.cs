b71e482b5aa94ec0058ae0435426b6216134ad2d
Dhiullian <dhiullian7seven@gmail.com>
2022-10-12 16:54:02 -0300

    update .net 6.0 e criacao API

---
+++ src/Api/CleanArch.BaseApi.Api/Program.cs
@@ -0,0 +1,43 @@
+using CleanArch.BaseApi.Application;
+using CleanArch.BaseApi.Infrastructure;
+using CleanArch.BaseApi.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+app.UseCors(c =>
+{
+    c.AllowAnyHeader();
+    c.AllowAnyMethod();
+    c.AllowAnyOrigin();
+});
+
+
+
+app.Run();

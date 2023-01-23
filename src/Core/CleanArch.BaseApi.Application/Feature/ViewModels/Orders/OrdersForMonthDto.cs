5ab0bdb726c303cfa1a85540bbfa7f816ad67a38
Dhiullian <dhiullian7seven@gmail.com>
2023-01-23 09:52:13 -0300

    Implements order controller and getDetails handler

---
+++ src/Core/CleanArch.BaseApi.Application/Feature/ViewModels/Orders/OrdersForMonthDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArch.BaseApi.Application.Feature.ViewModels.Orders
+{
+    public class OrdersForMonthDto
+    {
+        public Guid Id { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+    }
+}
--- src/Core/CleanArch.BaseApi.Application/Interfaces/Persistence/IOrderRepository.cs
+++ src/Core/CleanArch.BaseApi.Application/Interfaces/Persistence/IOrderRepository.cs
@@ -4,5 +4,7 @@ namespace CleanArch.BaseApi.Application.Interfaces.Persistence
 {
     public interface IOrderRepository : IBaseRepository<Order>
     {
+        Task<List<Order>> GetPagedOrdersForMonth(DateTime date, int page, int size);
+        Task<int> GetTotalCountOfOrdersForMonth(DateTime date);
     }
 }
